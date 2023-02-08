4e52ddf706520a723738ccf6087909f31538e87d
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-08 17:06:44 +0100

    Ejercicios 1 y 2 atualizados

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        System.out.println("a = " + a);
+        b = a + 5;
+        System.out.println("a = " + a + " y b = " + b);
+        a = 7;
+        System.out.println("a = " + a + " y b = " + b);
+    }
+
+
+}
